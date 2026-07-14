326c2c71c483360d43b82742bdda4d98dc5f34c2
hiteshi516 <hitugupta208@gmail.com>
2026-07-14 22:53:59 +0530

    fix: resolve querySrv connection issues by setting public DNS resolvers

--- seed/seedData.js
+++ seed/seedData.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const mongoose = require('mongoose');
 const Customer = require('../models/Customer');
